212f212fdd63ade50b8fad8a299a57797e3310d1
LeviMaycon <levioficial361@gmail.com>
2025-05-07 00:15:12 -0300

    feat(verse): add verse component to the main template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { VerseComponent } from "./features/verse-of-day/verse/verse.component";
+import { ApiService } from './core/services/api.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, VerseComponent],
   templateUrl: './app.component.html',
+  providers: [ApiService],
   styleUrl: './app.component.css'
 })
 export class AppComponent {
